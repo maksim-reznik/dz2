a4ec1564eb90c0600e5f2b90ec43883a669c3f75
maksim-reznik <77396857+maksim-reznik@users.noreply.github.com>
2021-11-20 23:39:26 +0300

    Update case3.py

--- case3.py
+++ case3.py
@@ -8,7 +8,9 @@ df.fillna(-1, inplace=True)
 x = df['Total Fat']
 z = df['Total Fat (% Daily Value)']
 
-print(f'Общего жира в одном проценте {x/z}')
+print(f'Общего жира в одном проценте дневной нормы {x/z}')
 s = pd.Series(data=x, index=z)
 s.plot()
 plt.show()
+
+#сколько общего жира в одном проценте дневной нормы
